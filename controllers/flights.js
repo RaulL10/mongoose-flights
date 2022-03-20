f090d6b86bacffb80a3a7b822b6faef3f3ae483a
RaulL10 <lopezraul1410@gmail.com>
2022-03-19 21:14:50 -0700

    list of flights now adding the newly created ones

--- controllers/flights.js
+++ controllers/flights.js
@@ -1,4 +1,5 @@
-const flight = require('../models/flight')
+const flight = require('../models/flight');
+const Flight = require('../models/flight');
 
 module.exports = {
     index,
@@ -6,19 +7,29 @@ module.exports = {
     create,
 }
 
+function newFlight(req, res) {
+    res.render('flights/new')
+}
+function create(req, res) {
+    // req.body.airline = 
+    // req.body.airport = req.body.airport
+    // // req.body.flightNo =
+    // // req.body.departs =
+    
+    console.log(req.body)
+    console.log(req.body.airline)
+    console.log()
+    console.log("i am creating")
+    const flight = new Flight(req.body)
+    flight.save(function(err) {
+        if (err) return res.render('flights/new');
+        res.redirect('/flights');
+    })
+}
+
 function index(req, res) {
     flight.find({}, function(err, flights) {
 
         res.render('flights', {flights})
     })
-}
-function newFlight(req, res) {
-    res.render('flights/new')
-}
-function create(req, res) {
-
-
-
-    console.log("i am creating")
-    res.redirect('/')
 }
--- models/flight.js
+++ models/flight.js
@@ -19,7 +19,7 @@ const flightSchema = new Schema ({
     departs: {
         type: Date,
         default: function () {
-            return new Date().getFullYear() + 1
+            return new Date().getFullYear()
         }
     }
 })
