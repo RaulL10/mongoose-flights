5b5c26eefdf35973106b8562a838c50377360c1e
RaulL10 <lopezraul1410@gmail.com>
2022-03-23 13:01:29 -0700

    added tickets create function

--- controllers/tickets.js
+++ controllers/tickets.js
@@ -1,17 +1,27 @@
 const Ticket = require('../models/ticket')
 const Flight = require('../models/flight')
+const mongoose = require('mongoose');
 
 
 module.exports = {
     new: newTicket,
+    create,
 }
 
 
 function newTicket(req, res) {
-    Ticket.find({}, function(err, flight) {
-        console.log('HEY')
-        res.render('tickets/new', {
-            flight
-        })
+    const flight = {id: req.params.id}
+    console.log('HEY')
+    res.render('tickets/new', {flight})
+}
+
+
+function create(req, res) {
+    const ticket = new Ticket(req.body)
+    ticket.flight = mongoose.Types.ObjectId(req.params.id)
+    ticket.save(function(err, tickets) {
+        console.log(tickets)
+        res.redirect('/flights/' + req.params.id)
+
     })
 }
